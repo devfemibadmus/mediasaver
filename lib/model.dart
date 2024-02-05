7526f63ce7ec76bf4c6539efb6180aa7ce8e887e
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-02-06 00:51:18 +0100

    :rotating_light: mergeVideoLists list validate and status action 1

--- lib/model.dart
+++ lib/model.dart
@@ -29,8 +29,11 @@ class StatusFileInfo {
       );
 }
 
-List<StatusFileInfo> parseStatusFiles(List<dynamic> files) =>
-    List.of(files.map((file) => StatusFileInfo.fromJson(file))).toList();
+List<StatusFileInfo> parseStatusFiles(List<dynamic> files) {
+  return files
+      .map((file) => StatusFileInfo.fromJson(Map<String, dynamic>.from(file)))
+      .toList();
+}
 
 List<StatusFileInfo> filterFilesByFormat(
   List<StatusFileInfo> files,
@@ -47,19 +50,62 @@ Future<String> statusAction(String filePath, String action) async {
   try {
     return await platform.invokeMethod(action, {'filePath': filePath});
   } on PlatformException catch (e) {
-    print("Error: ${e.message}");
     return "Error: ${e.message}";
   }
 }
 
-Future<bool> shareMedia(String filePath) async {
-  try {
-    return await platform.invokeMethod('shareMedia', {'filePath': filePath});
-  } on PlatformException catch (e) {
-    print("Error: ${e.message}");
+bool listsAreEqual(List<StatusFileInfo> list1, List<StatusFileInfo> list2) {
+  if (list1.length != list2.length) {
     return false;
   }
+
+  for (int i = 0; i < list1.length; i++) {
+    if (!statusFileInfoEquals(list1[i], list2[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool statusFileInfoEquals(StatusFileInfo info1, StatusFileInfo info2) {
+  return info1.name == info2.name &&
+      info1.path == info2.path &&
+      info1.size == info2.size &&
+      info1.format == info2.format &&
+      info1.source == info2.source;
 }
 
 List<String> images = ['jpg', 'jpeg', 'gif'];
 List<String> videos = ['mp4', 'mov', 'mp4'];
+
+List<StatusFileInfo> mergeVideoLists(
+  List<StatusFileInfo> currentList,
+  List<StatusFileInfo> newList,
+) {
+  // Create a set to efficiently check duplicates based on path and size
+  Set<String> currentSet = {};
+  for (var fileInfo in currentList) {
+    currentSet.add('${fileInfo.path}_${fileInfo.size}');
+  }
+
+  // Create a new list for the result
+  List<StatusFileInfo> resultList = [];
+
+  // Merge data from newList to resultList
+  for (var fileInfo in newList) {
+    if (currentSet.contains('${fileInfo.path}_${fileInfo.size}')) {
+      // Keep the mediaByte from the current list
+      var currentIndex = currentList.indexWhere((currentFile) =>
+          currentFile.path == fileInfo.path &&
+          currentFile.size == fileInfo.size);
+      if (currentIndex != -1) {
+        fileInfo.mediaByte = currentList[currentIndex].mediaByte;
+      }
+    }
+    resultList.add(fileInfo);
+  }
+  print('resultList.length');
+  print(resultList.length);
+  return resultList;
+}
