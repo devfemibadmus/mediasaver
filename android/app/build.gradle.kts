d793f01229636fdd2b0d5efb61d7bc0b876d7908
Dev Femi Badmus <devfemibadmus@gmail.com>
2026-05-27 23:34:04 +0100

    Update project metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.blackstackhub.mediasaver"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
