61df030a652ce53acde2278813c35cf5b49ab4c1
Dev Femi Badmus <devfemibadmus@gmail.com>
2026-05-28 03:01:17 +0100

    Auto redirect iOS shared links

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -1,12 +1,3 @@
 import receive_sharing_intent
 
-class ShareViewController: RSIShareViewController {
-  override func shouldAutoRedirect() -> Bool {
-    return false
-  }
-
-  override func presentationAnimationDidFinish() {
-    super.presentationAnimationDidFinish()
-    navigationController?.navigationBar.topItem?.rightBarButtonItem?.title = "Send"
-  }
-}
+class ShareViewController: RSIShareViewController {}
